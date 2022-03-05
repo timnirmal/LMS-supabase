68c3f6ee7f196d9af27b3ffb6f3483603cc997f7
timnirmal <timnirmal@gmail.com>
2022-03-05 07:49:39 +0530

    SlideShow Working Need to Clean Up

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,7 +9,7 @@ import Navbar from '../components/Navbar'
 import Example from './test'
 import Slider from "../components/Slidebar";
 import Image from 'next/image'
-import "flowbite"
+import Carousel from "../components/Carousel";
 
 // define the shape of the SignUp form's fields
 type SignUpFieldProps = {
@@ -59,71 +59,7 @@ const IndexPage: NextPage<NextAppPageProps> = ({}) => {
                 */}
 
 
-                <div id="default-carousel" data-carousel="slide" className="relative">
-                    <div className="overflow-hidden relative h-56 rounded-lg sm:h-64 xl:h-80 2xl:h-96">
-                        <div className="hidden duration-700 ease-in-out relative" data-carousel-item="active">
-                            <Image
-                                src="/Carosoul/carousel-1.svg"
-                                alt="Picture of the author"
-                                layout="fill"
-                                className="block absolute top-1/2 left-1/2 w-full -translate-x-1/2 -translate-y-1/2"
-                            />
-                        </div>
-                        <div className="hidden duration-700 ease-in-out relative" data-carousel-item="active">
-                            <Image
-                                src="/Carosoul/carousel-2.svg"
-                                alt="Picture of the author"
-                                layout="fill"
-                            />
-                        </div>
-                        <div className="hidden duration-700 ease-in-out relative" data-carousel-item="active">
-                            <Image
-                                src="/Carosoul/carousel-3.svg"
-                                alt="Picture of the author"
-                                layout="fill"
-                            />
-                        </div>
-                    </div>
-
-                    <div className="flex absolute bottom-5 left-1/2 space-x-3 -translate-x-1/2">
-                        <button type="button" className="w-3 h-3 bg-white rounded-full dark:bg-gray-800"
-                                aria-current="true" aria-label="Slide 1" data-carousel-slide-to="0"/>
-                        <button type="button"
-                                className="w-3 h-3 rounded-full bg-white/50 dark:bg-gray-800/50 hover:bg-white dark:hover:bg-gray-800"
-                                aria-current="false" aria-label="Slide 2" data-carousel-slide-to="1"/>
-                        <button type="button"
-                                className="w-3 h-3 rounded-full bg-white/50 dark:bg-gray-800/50 hover:bg-white dark:hover:bg-gray-800"
-                                aria-current="false" aria-label="Slide 3" data-carousel-slide-to="2"/>
-                    </div>
-
-
-                    <button type="button"
-                            className="flex absolute top-0 left-0 justify-center items-center px-4 h-full cursor-pointer group focus:outline-none" data-carousel-prev>
-                        <span className="inline-flex justify-center items-center w-8 h-8 rounded-full sm:w-10 sm:h-10 bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-                            <svg className="w-5 h-5 text-white sm:w-6 sm:h-6 dark:text-gray-800" fill="none" stroke="currentColor"
-                                 viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                                <path strokeLinecap="round"
-                                      strokeLinejoin="round" strokeWidth="2"
-                                      d="M15 19l-7-7 7-7"/></svg>
-                            <span className="hidden">Previous</span>
-                        </span>
-                    </button>
-
-                    <button type="button"
-                            className="flex absolute top-0 right-0 justify-center items-center px-4 h-full cursor-pointer group focus:outline-none"
-                            data-carousel-next>
-                        <span
-                            className="inline-flex justify-center items-center w-8 h-8 rounded-full sm:w-10 sm:h-10 bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-                            <svg className="w-5 h-5 text-white sm:w-6 sm:h-6 dark:text-gray-800" fill="none" stroke="currentColor"
-                                 viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                                <path strokeLinecap="round"
-                                      strokeLinejoin="round" strokeWidth="2"
-                                      d="M9 5l7 7-7 7"/></svg>
-                            <span className="hidden">Next</span>
-                        </span>
-                    </button>
-                </div>
-
+                <Carousel/>
 
 
             </Layout>
