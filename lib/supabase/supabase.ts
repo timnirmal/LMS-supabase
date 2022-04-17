9da03889aa94c01f9e3a822fddb4804045f2d47e
timnirmal <timnirmal@gmail.com>
2022-04-17 11:10:18 +0530

    Fixing env

--- lib/supabase/supabase.ts
+++ lib/supabase/supabase.ts
@@ -2,7 +2,7 @@ import { createClient } from '@supabase/supabase-js';
 
 const SUPABASE_URL: string = process.env.NEXT_PUBLIC_SUPABASE_URL || '';
 const SUPABASE_ANON_KEY: string = process.env.NEXT_PUBLIC_SUPABASE_KEY || '';
-const SUPABASE_SERVICE_KEY: string = process.env.NEXT_SERVICE_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRsd3hkbHhmcGhrZ2t5aWt3cnR4Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY0OTg0NTEzOCwiZXhwIjoxOTY1NDIxMTM4fQ.-nCtxwuYAkOo1j7GXpW9nhR4_MhUWlts-KV6k9jqRbc';
+const SUPABASE_SERVICE_KEY: string = process.env.NEXT_SERVICE_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRsd3hkbHhmcGhrZ2t5aWt3cnR4Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY1MDE2NzM2NywiZXhwIjoxOTY1NzQzMzY3fQ.zTzBzoUMAvdul7jbtL0dTE7wKnUxriIMQINV07McevI';
 
 const client = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
